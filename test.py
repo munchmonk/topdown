a8e6a58a13f014e99acf76eae047c914a9c2681b
jackie saltalepre <pirrulez@gmail.com>
2019-04-26 03:37:41 +0100

    map, walls

--- test.py
+++ test.py
@@ -4,22 +4,33 @@ import pygame
 import sys
 import math
 import random
-pygame.init()
+# pygame.init()
 
 # screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-screen = pygame.display.set_mode((100, 100))
+# screen = pygame.display.set_mode((100, 100))
 
 
-print(random.randint(0, 1))
-print(random.randint(0, 1))
-print(random.randint(0, 1))
-print(random.randint(0, 1))
-print(random.randint(0, 1))
-print(random.randint(0, 1))
-print(random.randint(0, 1))
+def point_in_blocks(x, y):
+	if (x in (2, 3, 4, 5)) or (x in (7, 8, 9)):
+		return True
+	return False
 
 
+def get_wall(x, y, wall=[]):
+	if not point_in_blocks(x, y):
+		return
+	if (x, y) in wall:
+		return
+	wall.append((x, y))
+	get_wall(x - 1, y, wall)
+	get_wall(x + 1, y, wall)
+	return wall
 
+print('#1', get_wall(3, 0, []))
+print('#2', get_wall(7, 0, []))
+
+
+"""
 while True:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
@@ -29,5 +40,4 @@ while True:
 			if event.key == pygame.K_ESCAPE:
 				pygame.quit()
 				sys.exit()
-
-	
+"""
--- topdown.py
+++ topdown.py
@@ -28,6 +28,280 @@ class Camera:
 		return pygame.Rect(obj_rect.x - self.x, obj_rect.y - self.y, obj_rect.width, obj_rect.height)
 
 
+class Wall(pygame.sprite.Sprite):
+	def __init__(self, game, x, y, width, height):
+		self.game = game
+		self.groups = game.allwalls
+		pygame.sprite.Sprite.__init__(self, self.groups)
+
+		self.rect = pygame.Rect(x, y, width, height)
+
+
+class Game:
+	pygame.init()
+	MAXZOMBIES = 20
+
+	def __init__(self):
+		self.screen = pygame.display.set_mode((0, 0))
+		self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+		self.screenwidth, self.screenheight = self.screen.get_size()
+		self.maxwidth, self.maxheight = 1500, 1200
+		self.clock = pygame.time.Clock()
+		self.dt = 0
+
+		self.allsprites = pygame.sprite.Group()
+		self.spritestoupdate = pygame.sprite.Group()
+		self.allwalls = pygame.sprite.Group()
+		self.allplayers = pygame.sprite.Group()
+		self.allblocks = pygame.sprite.Group()
+		self.allzombies = pygame.sprite.Group()
+		self.allbullets = pygame.sprite.Group()
+
+		self.joysticks = []
+
+		self.lastzombie = 0
+
+		self.camera = Camera(self)
+
+		self.setup()
+
+	def find_player(self, uid):
+		for player in self.allplayers:
+			if player.uid == uid:
+				return player
+		return None
+
+	def spawn_players(self):
+		for i in range(len(self.joysticks)):
+			while len(self.allplayers.sprites()) < i + 1:
+				player = Player(self, i, random.randint(1, self.maxwidth - 1), random.randint(1, self.maxheight - 1))
+				if pygame.sprite.spritecollide(player, self.allwalls, False):
+					player.kill()
+
+
+	def setup(self):
+		self.generate_map()
+		self.setup_joysticks()
+		self.spawn_players()
+
+	def spawn_zombies(self):
+		if (time.time() - self.lastzombie < 0.1) or (len(self.allzombies.sprites()) > Game.MAXZOMBIES):
+			return
+		zombie = None
+		while not zombie:
+			zombie = Zombie(self, random.randint(1, self.maxwidth - 1), random.randint(1, self.maxheight - 1))
+			if pygame.sprite.spritecollide(zombie, self.allwalls, False):
+				zombie.kill()
+				zombie = None
+			for player in self.allplayers:
+				if not zombie:
+					break
+				if((self.camera.apply(zombie).right >= 0) and
+					(self.camera.apply(zombie).left <= self.screenwidth) and
+					(self.camera.apply(zombie).bottom >= 0) and
+					(self.camera.apply(zombie).top <= self.screenheight)):
+					zombie.kill()
+					zombie = None
+		self.lastzombie = time.time()
+
+
+	def point_in_blocks(self, x, y):
+		for block in self.allblocks:
+			if block.rect.collidepoint(x, y):
+				return True
+		return False
+
+	def get_horiz_wall(self, x, y, wall=[]):
+		if not self.point_in_blocks(x, y):
+			return
+		if (x, y) in wall:
+			return
+		wall.append((x, y))
+		self.get_horiz_wall(x - Block.SIZE, y, wall)
+		self.get_horiz_wall(x + Block.SIZE, y, wall)
+		return wall
+
+	def get_vertic_wall(self, x, y, wall=[]):
+		if not self.point_in_blocks(x, y):
+			return
+		if (x, y) in wall:
+			return
+		wall.append((x, y))
+		self.get_vertic_wall(x, y - Block.SIZE, wall)
+		self.get_vertic_wall(x, y + Block.SIZE, wall)
+		return wall
+			
+		
+	def generate_walls(self):
+		# first round - horizontal walls
+		walls = []
+		
+		
+		for j in range(self.maxheight / Block.SIZE):
+			for i in range(self.maxwidth / Block.SIZE):
+				point_x = i * Block.SIZE + Block. SIZE // 2
+				point_y = j * Block.SIZE + Block.SIZE // 2
+
+				# Horizontal walls
+				wall = self.get_horiz_wall(point_x, point_y, [])
+				if wall and len(wall) > 1:
+					wall = sorted(wall)
+					walls.append(wall)
+
+				# Vertical walls
+				wall = self.get_vertic_wall(point_x, point_y, [])
+				if wall and len(wall) > 1:
+					wall = sorted(wall)
+					walls.append(wall)
+
+				# Single blocks
+				if self.point_in_blocks(point_x, point_y) and self.get_neighbours(i, j)[0] == 0:
+					walls.append([(point_x, point_y)])
+	
+
+		non_dup = []
+		for wall in walls:
+			if wall not in non_dup:
+				non_dup.append(wall)
+
+		print(len(non_dup), 'unique walls!')
+
+		for wall in non_dup:
+			# Single block
+			if len(wall) == 1:
+				Wall(self, wall[0][0] - Block.SIZE // 2, wall[0][1] - Block.SIZE // 2, Block.SIZE, Block.SIZE)
+
+			# Horizontal wall
+			elif wall[0][1] == wall[1][1]:
+				Wall(self, wall[0][0] - Block.SIZE // 2, wall[0][1] - Block.SIZE // 2, wall[-1][0] - wall[0][0] + Block.SIZE, Block.SIZE)
+
+			# Vertical wall
+			else:
+				Wall(self, wall[0][0] - Block.SIZE // 2, wall[0][1] - Block.SIZE // 2, Block.SIZE, wall[-1][1] - wall[0][1] + Block.SIZE)
+
+	
+	def get_neighbours(self, x, y):
+		ort_neighbours, diag_neighbours = 0, 0
+		for i in range(-1, 2):
+			for j in range(-1, 2):
+				if (i, j) == (0, 0):
+					continue
+				point_x = (i + x) * Block.SIZE + Block.SIZE // 2
+				point_y = (j + y) * Block.SIZE + Block.SIZE // 2
+				for block in self.allblocks:
+					if block.rect.collidepoint(point_x, point_y):
+						if i == 0 or j == 0:
+							ort_neighbours += 1
+						else:
+							diag_neighbours += 1
+		return ort_neighbours + diag_neighbours, ort_neighbours, diag_neighbours
+	
+
+	def generate_map(self):
+		# Generate outer grid
+		for i in range(self.maxwidth / Block.SIZE):
+			Block(self, i * Block.SIZE, 0)
+			Block(self, i * Block.SIZE, self.maxheight - Block.SIZE)
+		for i in range(1, self.maxheight / Block.SIZE - 1):
+			Block(self, 0, i * Block.SIZE)
+			Block(self, self.maxwidth - Block.SIZE, i * Block.SIZE)
+
+		
+		# First round, randomly spawn blocks near the edges
+		for i in range(1, self.maxwidth / Block.SIZE - 1):
+			for j in range(1, self.maxheight / Block.SIZE - 1):
+				point_x, point_y = i * Block.SIZE, j * Block.SIZE
+				if self.point_in_blocks(point_x, point_y):
+					continue
+				if self.get_neighbours(i, j)[1] == 1 and self.get_neighbours(i, j)[2] == 2:
+					if random.random() <= 0.1:
+						Block(self, point_x, point_y)
+
+
+		# Second round, randomly spawn blocks that have one orthogonal neighbour and no diagonal ones
+		for i in range(1, self.maxwidth / Block.SIZE - 1):
+			for j in range(1, self.maxheight / Block.SIZE - 1):
+				point_x, point_y = i * Block.SIZE, j * Block.SIZE
+				if self.point_in_blocks(point_x, point_y):
+					continue
+				if self.get_neighbours(i, j)[0] == 0:
+					if random.random() <= 0.005:
+						Block(self, point_x, point_y)
+
+		
+		# Second round, randomly spawn blocks that have one orthogonal neighbour and no diagonal ones
+		for i in range(1, self.maxwidth / Block.SIZE - 1):
+			for j in range(1, self.maxheight / Block.SIZE - 1):
+				point_x, point_y = i * Block.SIZE, j * Block.SIZE
+				if self.point_in_blocks(point_x, point_y):
+					continue
+				if self.get_neighbours(i, j)[1] == 1 and self.get_neighbours(i, j)[2] == 0:
+					if random.random() <= 0.85:
+						Block(self, point_x, point_y)
+		
+
+		print(len(self.allblocks.sprites()), 'blocks!')
+		self.generate_walls()
+
+
+	def setup_joysticks(self):
+		pygame.joystick.init()
+
+		for i in range(pygame.joystick.get_count()):
+			self.joysticks.append(pygame.joystick.Joystick(i))
+			self.joysticks[i].init()
+			# print(self.joysticks[i].get_name())
+
+	def quit(self):
+		pygame.quit()
+		sys.exit()
+
+	def run(self):
+		while True:
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					self.quit()
+				elif event.type == pygame.KEYDOWN:
+					if event.key == pygame.K_ESCAPE:
+						self.quit()
+
+			self.dt = self.clock.tick(60)
+			self.update()
+			self.draw()
+
+	def update(self):
+		self.spawn_zombies()
+		self.allsprites.update()
+		self.camera.update(self.find_player(0))
+		# print('fps   ', self.clock.get_fps())
+
+	def draw(self):
+		self.screen.fill((0, 200, 0))
+		for sprite in self.allsprites:
+			rect = self.camera.apply(sprite)
+			if rect.right < 0 or rect.bottom < 0 or rect.left > self.screenwidth or rect.top > self.screenheight:
+				continue
+			self.screen.blit(sprite.image, rect)
+
+
+
+
+		pygame.display.flip()
+
+
+class Block(pygame.sprite.Sprite):
+	IMG = pygame.image.load('block.png')
+	SIZE = 50
+
+	def __init__(self, game, x, y):
+		self.game = game
+		self.groups = game.allsprites, game.allblocks
+		pygame.sprite.Sprite.__init__(self, self.groups)
+
+		self.image = Block.IMG
+		self.rect = self.image.get_rect(topleft=(x, y))
+
+
 class Bullet(pygame.sprite.Sprite):
 	IMG = pygame.image.load('bullet.png')
 	SPEED = 1
@@ -35,7 +309,7 @@ class Bullet(pygame.sprite.Sprite):
 	def __init__(self, player, x, y):
 		self.game = player.game
 		self.player = player
-		self.groups = self.game.allsprites, self.game.allbullets
+		self.groups = self.game.allsprites, self.game.allbullets, self.game.spritestoupdate
 		pygame.sprite.Sprite.__init__(self, self.groups)
 
 		self.image = Bullet.IMG
@@ -53,6 +327,10 @@ class Bullet(pygame.sprite.Sprite):
 		self.rect.x += int(round(self.dx * 30))
 		self.rect.y += int(round(self.dy * 30))
 
+		if (self.rect.x < 0 or self.rect.right > self.game.maxwidth or self.rect.y < 0 or self.rect.bottom > self.game.maxheight or
+			pygame.sprite.spritecollide(self, self.game.allwalls, False)):
+			self.kill()
+
 
 	def update(self):
 		self.rect.x += int(round(self.dx * self.game.dt * Bullet.SPEED))
@@ -62,10 +340,8 @@ class Bullet(pygame.sprite.Sprite):
 			self.kill()
 
 		if (self.rect.x < 0 or self.rect.right > self.game.maxwidth or self.rect.y < 0 or self.rect.bottom > self.game.maxheight or
-			pygame.sprite.spritecollide(self, self.game.allblocks, False)):
+			pygame.sprite.spritecollide(self, self.game.allwalls, False)):
 			self.kill()
-
-		
 
 
 class Zombie(pygame.sprite.Sprite):
@@ -74,7 +350,7 @@ class Zombie(pygame.sprite.Sprite):
 
 	def __init__(self, game, x, y):
 		self.game = game
-		self.groups = game.allsprites, game.allzombies
+		self.groups = game.allsprites, game.allzombies, game.spritestoupdate
 		pygame.sprite.Sprite.__init__(self, self.groups)
 
 		self.image = Zombie.IMG
@@ -113,7 +389,7 @@ class Zombie(pygame.sprite.Sprite):
 				self.rect.x += 1
 			while self.rect.right > self.game.maxwidth:
 				self.rect.x -= 1
-			while pygame.sprite.spritecollide(self, self.game.allblocks, False):
+			while pygame.sprite.spritecollide(self, self.game.allwalls, False):
 				self.rect.x -= int(abs(dx) / dx)
 
 		if dy:
@@ -122,7 +398,7 @@ class Zombie(pygame.sprite.Sprite):
 				self.rect.y += 1
 			while self.rect.bottom > self.game.maxheight:
 				self.rect.y -= 1
-			while pygame.sprite.spritecollide(self, self.game.allblocks, False):
+			while pygame.sprite.spritecollide(self, self.game.allwalls, False):
 				self.rect.y -= int(abs(dy) / dy)
 
 	def get_angle_of_rotation(self):
@@ -163,7 +439,7 @@ class Zombie(pygame.sprite.Sprite):
 
 			# Adjust rotation so that it doesn't collide with blocks
 			new_alpha = alpha
-			while pygame.sprite.spritecollide(self, self.game.allblocks, False):
+			while pygame.sprite.spritecollide(self, self.game.allwalls, False):
 				# Decide whether to revert clockwise or counterclockwise
 				if (abs(alpha - self.angle)) < 180:
 					new_alpha = (new_alpha - 1) % 360
@@ -182,11 +458,6 @@ class Zombie(pygame.sprite.Sprite):
 		self.move()
 		self.rotate()
 
-		
-
-
-
-
 
 class Player(pygame.sprite.Sprite):
 	IMG = pygame.image.load('player.png')
@@ -194,7 +465,7 @@ class Player(pygame.sprite.Sprite):
 
 	def __init__(self, game, uid, x, y):
 		self.game = game
-		self.groups = game.allsprites, game.allplayers
+		self.groups = game.allsprites, game.allplayers, game.spritestoupdate
 		pygame.sprite.Sprite.__init__(self, self.groups)
 
 		self.image = Player.IMG
@@ -265,7 +536,7 @@ class Player(pygame.sprite.Sprite):
 
 			# Adjust rotation so that it doesn't collide with blocks
 			new_alpha = alpha
-			while pygame.sprite.spritecollide(self, self.game.allblocks, False):
+			while pygame.sprite.spritecollide(self, self.game.allwalls, False):
 				# Decide whether to revert clockwise or counterclockwise
 				if (abs(alpha - self.angle)) < 180:
 					new_alpha = (new_alpha - 1) % 360
@@ -289,7 +560,7 @@ class Player(pygame.sprite.Sprite):
 				self.rect.x += 1
 			while self.rect.right > self.game.maxwidth:
 				self.rect.x -= 1
-			while pygame.sprite.spritecollide(self, self.game.allblocks, False):
+			while pygame.sprite.spritecollide(self, self.game.allwalls, False):
 				self.rect.x -= int(abs(dx) / dx)
 
 		if dy:
@@ -298,7 +569,7 @@ class Player(pygame.sprite.Sprite):
 				self.rect.y += 1
 			while self.rect.bottom > self.game.maxheight:
 				self.rect.y -= 1
-			while pygame.sprite.spritecollide(self, self.game.allblocks, False):
+			while pygame.sprite.spritecollide(self, self.game.allwalls, False):
 				self.rect.y -= int(abs(dy) / dy)
 
 	def fire(self):
@@ -311,134 +582,6 @@ class Player(pygame.sprite.Sprite):
 		self.move()
 		self.fire()
 
-
-
-
-class Block(pygame.sprite.Sprite):
-	IMG = pygame.image.load('block.png')
-
-	def __init__(self, game, x, y):
-		self.game = game
-		self.groups = game.allsprites, game.allblocks
-		pygame.sprite.Sprite.__init__(self, self.groups)
-
-		self.image = Block.IMG
-		self.rect = self.image.get_rect(topleft=(x, y))
-
-	def update(self):
-		pass
-
-
-class Game:
-	pygame.init()
-	MAXZOMBIES = 30
-
-	def __init__(self):
-		self.screen = pygame.display.set_mode((0, 0))
-		# self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-		self.screenwidth, self.screenheight = self.screen.get_size()
-		self.maxwidth, self.maxheight = 3000, 3000
-		self.clock = pygame.time.Clock()
-		self.dt = 0
-
-		self.allsprites = pygame.sprite.Group()
-		self.allplayers = pygame.sprite.Group()
-		self.allblocks = pygame.sprite.Group()
-		self.allzombies = pygame.sprite.Group()
-		self.allbullets = pygame.sprite.Group()
-
-		self.joysticks = []
-		self.setup()
-
-		self.lastzombie = 0
-
-		for i in range(len(self.joysticks)):
-			Player(self, i, 500+100*i, 500+200*i)
-		self.camera = Camera(self)
-
-	def find_player(self, uid):
-		for player in self.allplayers:
-			if player.uid == uid:
-				return player
-		return None
-
-	def setup(self):
-		self.setup_map()
-		self.setup_joysticks()
-
-	def spawn_zombie(self):
-		if (time.time() - self.lastzombie < 0.1) or (len(self.allzombies.sprites()) > Game.MAXZOMBIES):
-			return
-		zombie = None
-		while not zombie:
-			zombie = Zombie(self, random.randint(1, self.maxwidth - 1), random.randint(1, self.maxheight - 1))
-			if pygame.sprite.spritecollide(zombie, self.allblocks, False):
-				zombie.kill()
-				zombie = None
-			for player in self.allplayers:
-				if not zombie:
-					break
-				if (abs(zombie.rect.x - player.rect.x) < self.screenwidth // 2 + 200 
-					or abs(zombie.rect.y - player.rect.y) < self.screenheight // 2 + 200):
-					zombie.kill()
-					zombie = None
-		self.lastzombie = time.time()
-
-	def setup_map(self):
-		Block(self, 0, 0)
-		Block(self, 100, 100)
-		Block(self, 150, 100)
-		Block(self, 150, 200)
-		Block(self, 200, 100)
-		Block(self, 750, 600)
-		Block(self, 800, 600)
-		Block(self, 800, 650)
-		Block(self, 1200, 800)
-		Block(self, 1550, 1150)
-		Block(self, 2950, 2950)
-
-	def setup_joysticks(self):
-		pygame.joystick.init()
-
-		for i in range(pygame.joystick.get_count()):
-			self.joysticks.append(pygame.joystick.Joystick(i))
-			self.joysticks[i].init()
-			print(self.joysticks[i].get_name())
-
-	def quit(self):
-		pygame.quit()
-		sys.exit()
-
-	def run(self):
-		while True:
-			for event in pygame.event.get():
-				if event.type == pygame.QUIT:
-					self.quit()
-				elif event.type == pygame.KEYDOWN:
-					if event.key == pygame.K_ESCAPE:
-						self.quit()
-
-			self.dt = self.clock.tick(60)
-			self.update()
-			self.draw()
-
-	def update(self):
-		self.spawn_zombie()
-		self.allsprites.update()
-		self.camera.update(self.find_player(0))
-
-	def draw(self):
-		self.screen.fill((0, 200, 0))
-		for sprite in self.allsprites:
-			rect = self.camera.apply(sprite)
-			if rect.right < 0 or rect.bottom < 0 or rect.left > self.screenwidth or rect.top > self.screenheight:
-				continue
-			self.screen.blit(sprite.image, rect)
-
-
-
-
-		pygame.display.flip()
 
 if __name__ == '__main__':
 	Game().run()
